79ee49b2d645851fb5d9a94f0a3c2b8c69bdacd4
Daniel Choong <danielchoong5@gmail.com>
2026-05-27 16:06:17 +0800

    feat(app): wire app entrypoint + theme

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sg_dividend/theme.dart';
+import 'package:sg_dividend/ui/splash_screen.dart';
+
+class SgDividendApp extends ConsumerWidget {
+  const SgDividendApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'SG Dividend Optimizer',
+      theme: AppTheme.light(),
+      home: const SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}

5ebeef8c479dba4cc17155039d1a88579513e6d4
Daniel Choong <danielchoong5@gmail.com>
2026-05-27 15:57:51 +0800

    chore(app): Flutter project scaffolding (pubspec + bundled fallback)

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sg_dividend/app.dart';
+
+void main() {
+  runApp(const ProviderScope(child: SgDividendApp()));
+}
